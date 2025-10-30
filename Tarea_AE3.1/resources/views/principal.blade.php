f7d551be2dc4f1eff587e46af3f7969a9e03c83d
Alberto <albertocarballogonzalez@alumno.ieselrincon.es>
2025-10-30 13:33:17 +0000

    nuevo blade

---
+++ Tarea_AE3.1/resources/views/principal.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Página de bienvenida</title>
+    </head>
+    <body>
+        <h1>Bienvenido a GymTime</h1>
+        <p>GymTime es una aplicación web dirigida a usuarios de gimnasios que desean reservar clases, 
+            sesiones de entrenamiento o el uso de instalaciones específicas en horarios determinados. 
+            Permite gestionar de manera sencilla las reservas, evitando sobrecargas y conflictos de horarios. 
+            La aplicación está pensada tanto para clientes como para administradores del gimnasio, ofreciendo un calendario interactivo, 
+            confirmaciones automáticas y control del aforo por franja horaria.
+        </p>
+
+        <button><a href="/login">Iniciar sesión</a></button>
+        <button><a href="/register">Registrarse</a></button>
+    </body>
+</html>
